ad3aec57aa954f20483ce65c62768f4dc421359d
Délia Cheminot <delia.cheminot@gmail.com>
2026-07-22 17:59:57 +0200

    Revert "fix: tone down card"
    This reverts commit 671b0b4132d99bd929a1efbe4e4c2baa97636723.

--- lib/ui/views/intakes/intake_counter_card.dart
+++ lib/ui/views/intakes/intake_counter_card.dart
@@ -24,7 +24,6 @@ class IntakeCounterCard extends StatelessWidget {
     return M3ECardColumn(
       padding: EdgeInsets.zero,
       margin: const EdgeInsets.symmetric(vertical: 8),
-      color: theme.colorScheme.surfaceContainerHigh,
       children: [
         ListTile(
           leading: CircleAvatar(
