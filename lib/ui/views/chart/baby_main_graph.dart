83e58245f772e95e22ff31dd23992ca14d29ddbc
Alice Lorido <alice@lori.do>
2026-08-23 18:09:01 +0200

    chore: remove unused padding (was used for testing)

--- lib/ui/views/chart/baby_main_graph.dart
+++ lib/ui/views/chart/baby_main_graph.dart
@@ -11,21 +11,12 @@ class BabyMainChartGraph extends StatelessWidget {
     if (spots.isEmpty) {
       return const SizedBox.shrink();
     }
-
-    final minX = spots.map((e) => e.x).reduce((a, b) => a < b ? a : b);
-    final maxX = spots.map((e) => e.x).reduce((a, b) => a > b ? a : b);
-    final maxY = spots.map((e) => e.y).reduce((a, b) => a > b ? a : b);
-    final xPadding = minX == maxX ? 1.0 : (maxX - minX) * 0.1;
-    final yPadding = maxY <= 0 ? 1.0 : maxY * 0.15;
     return SizedBox(
       height: 200,
       width: 96,
       child: LineChart(
         LineChartData(
-          minX: minX - xPadding,
-          maxX: maxX + xPadding,
           minY: 0,
-          maxY: maxY + yPadding,
           gridData: const FlGridData(show: false),
           borderData: FlBorderData(show: false),
           titlesData: _titlesData(),
