e8e44122662b1ff01cda0013680e1887dd5f7735
Alice Lorido <alice@lori.do>
2026-08-23 15:34:07 +0200

    chore: unused import

--- lib/ui/views/chart/bar_chart_graph.dart
+++ lib/ui/views/chart/bar_chart_graph.dart
@@ -2,7 +2,6 @@ import 'dart:math' as math;
 
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:mona/data/model/units.dart';
 
 class BarChartGraph extends StatelessWidget {
   final List<FlSpot> spots;
