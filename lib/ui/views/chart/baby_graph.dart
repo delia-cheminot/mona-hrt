ddb218455387042f316e5d0b3bbd5be05aeaae4e
Délia Cheminot <delia.cheminot@gmail.com>
2026-08-24 17:16:54 +0200

    refactor: simplify baby graph

--- lib/ui/views/chart/baby_graph.dart
+++ lib/ui/views/chart/baby_graph.dart
@@ -6,8 +6,6 @@ class BabyBarChartGraph extends StatelessWidget {
 
   BabyBarChartGraph({required this.spots});
 
-  static const double _barWidth = 10;
-  static const double _groupsSpace = 12;
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -15,16 +13,15 @@ class BabyBarChartGraph extends StatelessWidget {
       return const SizedBox.shrink();
     }
 
-    final maxY = spots.map((e) => e.y).reduce((a, b) => a > b ? a : b);
     return SizedBox(
       height: 48,
       width: 96,
       child: BarChart(
         curve: Curves.easeOutQuad,
         BarChartData(
-          maxY: maxY * 1.1,
+          maxY: spots.map((e) => e.y).reduce((a, b) => a > b ? a : b),
           alignment: BarChartAlignment.start,
-          groupsSpace: _groupsSpace,
+          groupsSpace: 12,
           barTouchData: BarTouchData(enabled: false),
           barGroups: _buildBarGroups(spots, theme),
           gridData: const FlGridData(
@@ -51,7 +48,7 @@ class BabyBarChartGraph extends StatelessWidget {
             barRods: [
               BarChartRodData(
                 toY: entry.value.y,
-                width: _barWidth,
+                width: 10,
                 color: entry.key == lastIndex
                     ? theme.colorScheme.tertiary
                     : theme.colorScheme.secondaryContainer,
@@ -62,13 +59,5 @@ class BabyBarChartGraph extends StatelessWidget {
         .toList();
   }
 
-  FlTitlesData _titlesData() => FlTitlesData(
-        show: true,
-        bottomTitles:
-            const AxisTitles(sideTitles: SideTitles(showTitles: false)),
-        leftTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
-        topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
-        rightTitles:
-            const AxisTitles(sideTitles: SideTitles(showTitles: false)),
-      );
+  FlTitlesData _titlesData() => FlTitlesData(show: false);
 }
