864601610b5392cf148509cc857a6f5a2a183aa3
Délia Cheminot <delia.cheminot@gmail.com>
2026-08-25 11:45:55 +0200

    fix: remove duplicate sizing

--- lib/ui/views/chart/baby_graph.dart
+++ lib/ui/views/chart/baby_graph.dart
@@ -14,29 +14,25 @@ class BabyBarChartGraph extends StatelessWidget {
       return const SizedBox.shrink();
     }
 
-    return SizedBox(
-      height: 48,
-      width: 96,
-      child: BarChart(
-        curve: Curves.easeOutQuad,
-        BarChartData(
-          maxY: spots.map((e) => e.y).reduce((a, b) => a > b ? a : b),
-          alignment: BarChartAlignment.spaceEvenly,
-          groupsSpace: 10,
-          barTouchData: BarTouchData(enabled: false),
-          barGroups: buildBarGroups(
-            spots,
-            barWidth: 10,
-            highlightedIndex: spots.length - 1,
-            barColor: theme.colorScheme.secondaryContainer,
-            highlightColor: theme.colorScheme.tertiary,
-          ),
-          gridData: const FlGridData(
-            show: false,
-          ),
-          titlesData: FlTitlesData(show: false),
-          borderData: FlBorderData(show: false),
+    return BarChart(
+      curve: Curves.easeOutQuad,
+      BarChartData(
+        maxY: spots.map((e) => e.y).reduce((a, b) => a > b ? a : b),
+        alignment: BarChartAlignment.spaceEvenly,
+        groupsSpace: 10,
+        barTouchData: BarTouchData(enabled: false),
+        barGroups: buildBarGroups(
+          spots,
+          barWidth: 10,
+          highlightedIndex: spots.length - 1,
+          barColor: theme.colorScheme.secondaryContainer,
+          highlightColor: theme.colorScheme.tertiary,
         ),
+        gridData: const FlGridData(
+          show: false,
+        ),
+        titlesData: FlTitlesData(show: false),
+        borderData: FlBorderData(show: false),
       ),
     );
   }
